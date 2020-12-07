00910a6e250ee455c27525680a0be976915bd20c
Elbert Bae <elbert.bae@gmail.com>
2020-12-07 13:59:20 -0800

    GEC-3 correctly setup rootAsync for mongoose module to access process.env through nestjs/config module gadd

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,18 +1,25 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { UserModule } from './users/user.module'
 
+console.log(process.env.MONGODB_URL);
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest'),
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql'
     }),
-    UserModule
+    UserModule,
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.MONGODB_URL
+      })
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
