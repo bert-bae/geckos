a31c34a8dcf867211fa32fa6a4225409ea40a9d5
Elbert Bae <elbert.bae@gmail.com>
2021-01-01 16:59:24 -0800

    GEC-25 run lerna lint

--- packages/server/src/geck-tasks/geck-tasks.module.ts
+++ packages/server/src/geck-tasks/geck-tasks.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { GeckTasksResolver } from './geck-tasks.resolver';
-import { GeckTasksService } from './geck-tasks.service'
-import { GeckTaskSchema } from './geck-tasks.schema'
+import { GeckTasksService } from './geck-tasks.service';
+import { GeckTaskSchema } from './geck-tasks.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'GeckTask', schema: GeckTaskSchema }])],
-  providers: [GeckTasksService, GeckTasksResolver],
+  imports: [
+    MongooseModule.forFeature([{ name: 'GeckTask', schema: GeckTaskSchema }])
+  ],
+  providers: [GeckTasksService, GeckTasksResolver]
 })
 export class GeckTasksModule {}
