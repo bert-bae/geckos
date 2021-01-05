7f3320bb49d59239d77f815cf1d25181bc3ccff5
Elbert Bae <elbert.bae@gmail.com>
2021-01-04 21:27:17 -0800

    GEC-32 change back app.js

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,21 +1,5 @@
-import AccordionTask from './components/accordion-task';
-
 function App() {
-  return (
-    <div>
-      <AccordionTask
-        TaskSummaryProps={{
-          title: 'hello',
-          type: 'Task',
-          link: 'https://www.google.ca'
-        }}
-        TaskDetailProps={{
-          type: 'Task',
-          description: 'hello world first description'
-        }}
-      />
-    </div>
-  );
+  return <div>Hello world!</div>;
 }
 
 export default App;
