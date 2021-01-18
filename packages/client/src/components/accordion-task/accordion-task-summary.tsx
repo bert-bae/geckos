fe43832c28910601d335cd7bce95f415f8fb4b32
Elbert Bae <elbert.bae@gmail.com>
2021-01-17 18:24:37 -0800

    GEC-32 use generated type for enum

--- packages/client/src/components/accordion-task/accordion-task-summary.tsx
+++ packages/client/src/components/accordion-task/accordion-task-summary.tsx
@@ -3,9 +3,10 @@ import { AccordionLinkSummary } from '../../shared-components/accordion';
 import AssignmentRoundedIcon from '@material-ui/icons/AssignmentRounded';
 import BugReportRoundedIcon from '@material-ui/icons/BugReportRounded';
 import ListAltRoundedIcon from '@material-ui/icons/ListAltRounded';
+import { GeckTaskTypes } from '../../types';
 
 export interface AccordionTaskSummaryProps {
-  type: 'Task' | 'Epic' | 'Bug';
+  type: GeckTaskTypes;
   title: string;
   link?: string;
   onLinkClick?: () => void;
@@ -14,7 +15,8 @@ export interface AccordionTaskSummaryProps {
 const taskIcons = {
   Task: <AssignmentRoundedIcon fontSize="small" />,
   Epic: <ListAltRoundedIcon fontSize="small" />,
-  Bug: <BugReportRoundedIcon fontSize="small" />
+  Bug: <BugReportRoundedIcon fontSize="small" />,
+  Root: null
 };
 
 const AccordionTaskSummary: React.FC<AccordionTaskSummaryProps> = ({
