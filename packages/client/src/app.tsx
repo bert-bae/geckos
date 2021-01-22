af64fcb45da138fa60b05b83e1ec29e7193ccbbb
Adam Romeo <adam.r.romeo@gmail.com>
2021-01-21 22:38:06 -0800

    Remove dummy container and update index.tsx

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { ApolloProvider } from '@apollo/client';
 import { client } from './apollo';
+import TaskPage from 'pages/task';
 import Container from './Container';
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Container />
+      <TaskPage />
     </ApolloProvider>
   );
 }
