e9afc8ec478885e4bcdf692fc750fc37f405bdc9
Adam Romeo <adam.r.romeo@gmail.com>
2021-01-22 10:25:42 -0800

    Change App into arrow function

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -3,12 +3,10 @@ import { ApolloProvider } from '@apollo/client';
 import { client } from './apollo';
 import TaskPage from 'pages/task';
 
-function App() {
-  return (
-    <ApolloProvider client={client}>
-      <TaskPage />
-    </ApolloProvider>
-  );
-}
+const App = () => (
+  <ApolloProvider client={client}>
+    <TaskPage />
+  </ApolloProvider>
+);
 
 export default App;
