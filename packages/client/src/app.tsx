708fa142e3cd551ffc090fd73dbcb90855db0bff
Adam Romeo <adam.r.romeo@gmail.com>
2021-01-22 10:23:43 -0800

    Create dummy page for submitting new tasks

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { ApolloProvider } from '@apollo/client';
 import { client } from './apollo';
 import TaskPage from 'pages/task';
-import Container from './Container';
 
 function App() {
   return (
--- packages/client/src/features/task/task-details/task-details.tsx
+++ packages/client/src/features/task/task-details/task-details.tsx
@@ -1,3 +1,53 @@
-const TaskDetails = () => <div>Task Details</div>;
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+// This component is temporary as an example of using the generated hooks
+
+type CreateTaskFormState = {
+  title: string;
+  description: string;
+  tags: string[];
+};
+
+const formInitialState: CreateTaskFormState = {
+  title: '',
+  description: '',
+  tags: []
+};
+
+const TaskDetails = () => {
+  const [formState, setFormState] = React.useState<CreateTaskFormState>(
+    formInitialState
+  );
+
+  const handleFormChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setFormState((prevFormState) => ({
+      ...prevFormState,
+      [event.target.name]: event.target.value
+    }));
+
+  const handleFormSubmit = () => {
+    console.log('Attempting to save the form...');
+  };
+
+  return (
+    <form>
+      <TextField
+        label="title"
+        name="title"
+        onChange={handleFormChange}
+        value={formState.title}
+      />
+      <TextField
+        label="description"
+        name="description"
+        onChange={handleFormChange}
+        value={formState.description}
+      />
+      <Button onClick={handleFormSubmit}>Submit</Button>
+    </form>
+  );
+};
 
 export default TaskDetails;
