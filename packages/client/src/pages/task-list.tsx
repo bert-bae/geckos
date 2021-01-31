5983ffc5c888179c7f70aaaa04f1e64dd6355e2f
Adam Romeo <adam.r.romeo@gmail.com>
2021-01-31 14:37:33 -0800

    Use page layout on task list

--- packages/client/src/pages/task-list.tsx
+++ packages/client/src/pages/task-list.tsx
@@ -1,14 +1,15 @@
 import { Link } from 'react-router-dom';
 import { ISubRoute, withSubRouter } from 'utils/router';
+import PageWithHeader from 'features/common/layout/page-with-header';
 import { getLink } from 'utils/link';
 
 interface TaskListPageProps extends ISubRoute {}
 
 const TaskListPage = (props: TaskListPageProps) => (
-  <>
+  <PageWithHeader>
     <Link to={getLink.login()}>a</Link>
     <div>Task List Page Placeholder</div>
-  </>
+  </PageWithHeader>
 );
 
 export default withSubRouter(TaskListPage);
