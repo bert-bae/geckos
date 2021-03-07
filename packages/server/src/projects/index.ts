964b99190ca053c5b8656ef02f0854f7930876e4
Adam Romeo <adam.r.romeo@gmail.com>
2021-03-07 13:55:14 -0800

    Create indexes for exports

---
+++ packages/server/src/projects/index.ts
@@ -0,0 +1,9 @@
+import { ProjectsService } from './projects.service';
+
+export * from './projects.service';
+
+export * from './projects.module';
+
+export * from './projects.model';
+
+export * from './projects.schema';
